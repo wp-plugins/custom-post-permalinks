6478f6f3a11327bfd34267ca891fc00eab00fef2
JohnPBloch <JohnPBloch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-30 00:23:32 +0000

    Adding an uninstall routine. Flushes rewrite rules on uninstallation, resetting any existing post types that would have been touched by the plugin.
    git-svn-id: https://plugins.svn.wordpress.org/custom-post-permalinks/trunk@295056 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,20 @@
+<?php
+
+delete_option( 'jpb_custom_post_permalinks_settings' );
+
+delete_option( 'jpb_custom_post_permalinks_version' );
+
+global $wp_rewrite;
+
+$post_types = get_post_types( array( '_builtin' => false, 'publicly_queryable' => true ), 'object' );
+
+foreach( $post_types as $pt => $t ){
+	if( false == $t->rewrite && isset($wp_rewrite->extra_permastructs[$pt] )
+		unset( $wp_rewrite->extra_permastructs[$pt] );
+	elseif( false != $t->rewrite )
+		$wp_rewrite->add_permastruct( $pt, "{$t->rewrite['slug']}/%$pt%", $t->rewrite['with_front'], $t->permalink_epmask );
+}
+
+$wp_rewrite->flush_rules( true );
+
+?>
