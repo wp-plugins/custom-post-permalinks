b2b267dc60b5d040d45db5efc1f4069adb535b26
JohnPBloch <JohnPBloch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-30 00:26:42 +0000

    Quick fix
    git-svn-id: https://plugins.svn.wordpress.org/custom-post-permalinks/trunk@295057 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -9,7 +9,7 @@ global $wp_rewrite;
 $post_types = get_post_types( array( '_builtin' => false, 'publicly_queryable' => true ), 'object' );
 
 foreach( $post_types as $pt => $t ){
-	if( false == $t->rewrite && isset($wp_rewrite->extra_permastructs[$pt] )
+	if( false == $t->rewrite && isset($wp_rewrite->extra_permastructs[$pt] ) )
 		unset( $wp_rewrite->extra_permastructs[$pt] );
 	elseif( false != $t->rewrite )
 		$wp_rewrite->add_permastruct( $pt, "{$t->rewrite['slug']}/%$pt%", $t->rewrite['with_front'], $t->permalink_epmask );
